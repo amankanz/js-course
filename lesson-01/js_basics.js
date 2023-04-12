4fa7ba83bdec2c03433bb50510e2f2bdcd33736d
amanikaneza <akanezamulume.iuea@gmail.com>
2023-04-12 10:14:14 +0300

    js course

--- lesson-01/js_basics.js
+++ lesson-01/js_basics.js
@@ -1,12 +1,26 @@
 //Lesson 01 - JavaScript Basics
 
+/**
+ * We can use javascript to :
+ * Give instruction to a computer
+ * Do Math
+ * Modify the webpage (important feature of JavaScript)
+ */
+
 /*
   a. Instruction to create a pop up:
 
   == alert('hello'); // create a pop up with text 'hello' inside.
   == alert('Good JOb'); // create a pop up ith text 'Good JOb' inside.
 
+  b. Math
 
+  == console.log(2 + 2); // The computer calculates the math and then gives us the result which is 4.
 
+  ==console.log(10 - 3); // THe computer does the math then gives us the reult which is 7 
+
+  c. Use JavaScript to modify the Webpage (important feature of JavaScript)
+
+  == document.body.innerHTML = "Good Job"; // This Code removes everything on the webpage and replaces it with the string 'Good Job'. 
 
 */
