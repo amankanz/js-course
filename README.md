3abdd2473fbcd06e537d0224156273348a5bdbc7
amanikaneza <akanezamulume.iuea@gmail.com>
2023-04-12 09:15:02 +0300

    js course

---
+++ README.md
@@ -0,0 +1,5 @@
+# js-course
+
+This repo contains the source code of JavaScript course undertaking!
+
+Care to leave a comment!
---
+++ lesson-01/js_basics.js
@@ -0,0 +1,12 @@
+//Lesson 01 - JavaScript Basics
+
+/*
+  a. Instruction to create a pop up:
+
+  == alert('hello'); // create a pop up with text 'hello' inside.
+  == alert('Good JOb'); // create a pop up ith text 'Good JOb' inside.
+
+
+
+
+*/
