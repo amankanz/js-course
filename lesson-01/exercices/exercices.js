bda5a76c1b01ebb375bbab48a16fad5432ea244b
amanikaneza <akanezamulume.iuea@gmail.com>
2023-04-13 09:41:35 +0300

    js course

---
+++ lesson-01/exercices/exercices.js
@@ -0,0 +1,20 @@
+/*
+== Lesson 1 Exercices - 1c
+
+console.log(10 + 5); // The computer calculates the math then gives us the result which is 15.
+
+== Lesson 1 Exercices - 1d
+
+console.log(20 - 5); // The computer calculates the math then gives us the result whcih is 15.
+
+== Lesson 1 Exercices - 1e
+
+console.log(2 + 2 - 5); // The computer calculates the math then gives us the result which is -1 
+
+
+
+
+
+
+
+*/
