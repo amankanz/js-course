310964e8b7942871afb7a6164e048b34526509dc
amanikaneza <akanezamulume.iuea@gmail.com>
2023-04-16 08:10:41 +0300

    js course

---
+++ lesson -02/js-basics.js
@@ -0,0 +1,32 @@
+/*
+== Add to cart: ==
+
+2 quantity of socks at $10.90 with FREE shipping, and 1 quantity of Basketball at $20.95 with $4.99 - shipping.
+
+== E.g - 1:
+
+items (3):  $42.75 // cost of product bef taxes and shipping.
+
+console.log(10.9 * 2 + 20.95); // calculated using Javascript.
+
+== E.g - 2:
+
+Total bef tax:  $47.74 // cost of procust + shipping.
+
+console.log(10.9 * 2 + 20.95 + 4.99); // result is 47.74 calculated using JS.
+
+
+== Order of operations (operator precedence) ==
+
+* / == are done 1st
+- + == are done after
+
+2 + 2; 2 - 2; 10 * 3; 10 /2; // These are called operations. THe chracters in the middle are called operators.
+
+
+
+
+
+
+
+*/
--- lesson-01/exercices/exercices.js
+++ lesson-01/exercices/exercices.js
@@ -11,10 +11,18 @@ console.log(20 - 5); // The computer calculates the math then gives us the resul
 
 console.log(2 + 2 - 5); // The computer calculates the math then gives us the result which is -1 
 
+== Lesson 1 Exercices - 1f
 
+document.body.innerHTML = "Good morning!"; // This code modify / remove the content of the web page and replaces it by the string 'Good morning!'. 
 
+== Lesson 1 Exo - 1g
 
+document.body.innerHTML = 'Amani Zihalirwa Khalid.'; //This code remove the content of the web page and replaces it by the string 'Amani Zihalirwa Khalid.';
+
+== Challenge Exercices ==
 
 
 
 */
+
+console.log("$" + 10 + "$" + 8 + "$" + 20);
