33b419f19efa3ad04a37ac5d7b69e6ece24686ba
amanikaneza <akanezamulume.iuea@gmail.com>
2023-04-22 09:21:05 +0300

    js course

---
+++ lesson-03/string.js
@@ -0,0 +1,28 @@
+/*
+== Strings ==
+
+A string represents text.
+
+console.log("hello"); // This code display a string that represents the text 'hello'.
+
+alert("hello"); //THis code creates a pop-up on the page.
+
+== Concantenation ==
+
+Is when we combine strings together.
+
+console.log("some" + "text"); // code displays 'sometext'
+
+console.log("some" + "more" + "text");
+ // code display 'somemoretext'
+
+
+== Types of values in JS ==
+
+Numbers and strings are two different types in javascript.
+
+
+
+
+*/
+console.log(typeof "amani");
