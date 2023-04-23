d5e0efa4344a48c5fcb340c996d8582d9ccebca2
amanikaneza <akanezamulume.iuea@gmail.com>
2023-04-23 13:08:49 +0300

    js course

---
+++ lesson-03/exercices/exercices_lesson3.js
@@ -0,0 +1,74 @@
+/*
+== Lesson 1 Exo - 3a ==
+
+Create a text 'My name is:' as a string.
+
+console.log("My name is:"); // displays in the console a the strings My name is:
+
+== Lesson 1 Exo - 3b ==
+
+Create your name as a string.
+
+console.log("Ziha Amani"); // displays in the console the strings Ziha Amani
+
+
+== Lesson 1 Exo - 3c ==
+
+Using concatenation, add 2 strings from exo 3a ('My name is:') and 3b ('Ziha Amani') together to create one text.
+
+console.log("My name is:" + "Ziha Amani"); // displays in the console the strings My name is:Ziha Amani
+
+
+== Lesson 1 Exo - 3d ==
+
+Calculate the total cost for 1 coffee for $5 and 1 bagel for $3. Using concatenation, to create the text.
+
+console.log("Total cost: $" + (5 + 3)); // displays the result in the console Total cost: $8
+
+== Lesson 1 Exo - 3e ==
+
+Calculate the total cost for 1 coffee for $5 and 1 bagel for $3. Using template string and interpolation, to create the text.
+
+console.log(`Total cost: $${5 + 3}`); // displays the result in the console Total cost: $8
+
+
+== Lesson 1 Exo - 3f ==
+
+Display the text 'Total cost: $8' in a pop-up
+
+alert(`Total cost: $${5 + 3}`); // displays a pop-up inside with the text Total cost: $8 
+
+
+== Lesson 1 Exo - 3g ==
+
+Calculate the total cost of 1 coffee for $5.99, 1 bagel for $2.95. Using concatenation, create the text 'Total cost: $_'
+
+console.log("Total cost: $" + (599 + 295) / 100); // the displays in the console Total cost: $8.94
+
+
+== Lesson 1 Exo - 3h ==
+
+Calculate the total cost of 1 coffee for $5.99, 1 bagel for $2.95. Using template string and interpolation, create the text 'Total cost: $_'
+
+console.log(`Total cost: $${(599 + 295) / 100}`); // the displays in the console Total cost: $8.94
+
+
+== Lesson 1 Exo - 3i ==
+
+Display the text (Total cost: $8.94) in the popup
+
+alert(`Total cost: $${(599 + 295) / 100}`); // display inside the popup the text Total cost: $8.94
+
+
+== Lesson 1 Exo - 3j ==
+
+Create the text from 3h (Total cost: $8.94) and add line text underneath it that says: 'Thank you, come again!' Dsiplay both lines in the pop-up
+
+alert(`Total cost: $${(599 + 295) / 100}
+Thank you, come again!`); // displays a multi-line string inside the pop-up that says Total cost: $8.94
+          THank you, come again on a new line.
+
+
+
+
+*/
--- lesson-03/string.js
+++ lesson-03/string.js
@@ -21,8 +21,97 @@ console.log("some" + "more" + "text");
 
 Numbers and strings are two different types in javascript.
 
+== Type coercion ==
+
+Type coercion (automatic type conversion) 
+
+console.log("hello" + 2); // Adding a string and a number JavaScript will automaticaly convert the number(2) into a string then combines them together 'hello' + '2' the result become 'hello2'
+
+== String also follow the order of operation ==
+
+console.log("$" + (2095 + 799) / 100); // THe result is $28.94
+
+== Strings and numbers ==
+
+  = Concatination
+
+console.log("Items (" + (1 + 1) + ") :  $" + (2095 + 799) / 100); // The result is 'Items (2): $28.94'
+
+
+alert("Items (" + (1 + 1) + "):  $" + (2095 + 799) / 100); // Code creates a pop-up inside our page that says Items (2):  $28.94 
+
+== 3 ways to create a string ==
+
+1 = We create string by using single quotes '...' (recommended in JavaScript)
+
+e.g console.log('hello'); // displays a string hello in the console
+
+2 = We create string by using double-quote "..."
+
+e.g console.log("hello"); // displays a string hello in the console
+
+= Special case to use double-quotes =
+
+console.log("I'm learning JavaScript"); // displays stings that says I'm learning JavaScript 
+
+The above exo shows single-quote inside double-quotes.
+
+= Escape Character =
+
+character = 1 letter / number / symbol
+
+'hello' has 5 chracter.
+
+CHaracter can be :
+
+  - a number (1, 2, 3)
+  -  a letter (a, b, c)
+  - a symbol (!, @, #)
+  - Escape character (\') (special character to be used in strings)
+
+console.log('I\'m learning JavaScript');
+
+= Newline Character =
+
+Newline character (\n) //creates a new line of texts
+
+alert("some\ntext"); //creates a pop-up with the string some and text on the newline 
+
+3 = We create string by using back-ticks `...`
+
+e.g console.log(`hello`); // displays a string hello in the console
+
+= Template strings =
+
+Strings created with back-ticks are called template strings.
+
+  = Special features of back-ticks =
+
+= Interpolation 
+
+Interpolation lets us insert a value directly into a string
+
+${} //allows us to insert a value directly into a string
+
+console.log(`Items (${1 + 1}): $${(2095 + 799) / 100}`); // result is Items (2): $28.94 inside the console
+
+Interpolation is much more cleaner way  to insert value into a string (recommended)
+
+= Multi-line string
+
+console.log(`some
+text`); // display strings some and text on the newline
+
+== WHat to use to create a strin ==
+
+1= Use '...' by default
+
+2= If we need interpolation or mult-line string use `...` (template string)
+
 
 
 
 */
-console.log(typeof "amani");
+
+console.log(`some
+text`);
